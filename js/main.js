4b07a0d2b1df33ae689589d1f8bbae236b13e6b7
li.li <li.li@ele.me>
2015-05-11 18:23:43 +0800

    react-router so weird, confused

--- js/main.js
+++ js/main.js
@@ -6,10 +6,7 @@ let Todo = require('./components/Todo');
 
 let TodoAction = require('./actions/TodoAction');
 
-let render = (route) => {
-  TodoAction.receiveAll(route);
-  React.render(<Todo route={route} />, document.getElementById('todoapp'));
-};
+
 
 
 
@@ -20,9 +17,30 @@ c.addEventListener('click', function(e) {
   e.stopPropagation();
 });*/
 
+let render = (route) => {
+  TodoAction.receiveAll(route);
+  React.render(<Todo route={route} />, document.getElementById('todoapp'));
+};
+
 render(global.location.hash.substr(2));
 
 
 global.addEventListener('hashchange', () => {
   render(global.location.hash.substr(2));
 });
+
+/*
+let Router = require('react-router');
+let {Route, RouteHandle, HashLocation} = Router;
+
+
+let routes = (
+  <Route path="/:route" handler={Todo}>
+  </Route>
+);
+
+
+
+Router.run(routes, (Handle) => {
+  React.render(<Handle />, document.getElementById('todoapp'));
+});*/
